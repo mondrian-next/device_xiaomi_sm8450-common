ca204fd0dd82a61cafb952a66a05f37d1544e49a
Arian <arian.kulmer@web.de>
2024-03-14 00:14:05 +0530

    sm8450-common: parts: Import from xiaomi_sdm845-common
    https://github.com/LineageOS/android_device_xiaomi_sdm845-common/tree/e261035d91b34cf0fdf0f012927e06a2bcdb03ed/parts
    
    Change-Id: I442f48780ca020f3e420ee4ccaf8ac07b05457cb
    Signed-off-by: Anush02198 <anush.4376@gmail.com>

--- common.mk
+++ common.mk
@@ -158,6 +158,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
     libdrm.vendor
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Dumpstate
 PRODUCT_PACKAGES += \
     android.hardware.dumpstate@1.1.vendor
