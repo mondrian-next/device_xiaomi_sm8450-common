b530b8ce8f1957339a2bd368cce0b43f5bb0a46b
bheatleyyy <sugakesshaa@pixelos.net>
2024-03-15 20:06:59 +0100

    sm8450-common: Add bluetooth a2dp audio routes to audio policy config
    needed for dolby atmos playback
    
    Change-Id: I1e60db9117f00bb70511726012d739ae1f687b09

--- common.mk
+++ common.mk
@@ -88,6 +88,7 @@ $(foreach sku, taro diwali cape ukee parrot, \
     ))
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/bluetooth_hearing_aid_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/bluetooth_hearing_aid_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
