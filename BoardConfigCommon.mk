791170782526370c9049ea1a6ad5e6a653612102
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-03-20 10:20:34 +0800

    sm8450-common: Optimize native executables for Cortex-A510
    Enable compiler optimizations for ARMv9 Cortex-A510 CPU.
    Needs https://gerrit.aospa.co/q/topic:topaz-armv9
    
    Change-Id: I98a9815c19c83b6897f21667b27af0723e8f74b0

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -42,7 +42,7 @@ TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a76
+TARGET_2ND_CPU_VARIANT := cortex-a55
 
 # Audio
 $(call soong_config_set, android_hardware_audio, run_64bit, true)
