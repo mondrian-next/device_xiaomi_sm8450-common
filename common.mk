f9c0ece9187adfab3acee85fb1adc659c9f0a0cb
Albert I <kras@raphielgang.org>
2024-04-15 00:05:08 +0200

    sm8450-common: config: Inherit several Android Go configurations
    * Beneficial for everyone, even on non-Go devices.
    
    Change-Id: I088fd00500ce10df5f413cb762aaed2ec00f5dfc
    Signed-off-by: Christopher <mizdrake7@gmail.com>

--- common.mk
+++ common.mk
@@ -483,6 +483,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
+# Improve performance
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
