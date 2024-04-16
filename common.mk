7a6767723bda1a47c6e5cbdd0e729f081f49fe2e
Mathieu Chartier <mathieuc@google.com>
2024-04-16 12:02:34 +0200

    sm8450-common: Speed profile services and wifi-service to reduce RAM and storage
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- common.mk
+++ common.mk
@@ -535,6 +535,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
