fb0bc3b5b92cdc565bedfe1d53f38d63262ec508
henricaodopao <allricknation@gmail.com>
2024-04-20 15:58:37 +0200

    sm8450-common: don't build QTI power hal
    It doesn't work
    
    * E QTI PowerHAL: Failed to perform launch boost
    * E QTI PowerHAL: Failed to acquire lock.

--- common.mk
+++ common.mk
@@ -379,7 +379,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service
 
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/power/config/taro/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
